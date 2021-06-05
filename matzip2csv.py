1578c07923c55694c36933299e95e4079b3c9535
HJJ <hwangjaejun525@gmail.com>
2021-06-05 13:33:42 +0900

    first commit test

---
+++ matzip2csv.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import requests 
+client_id = "INgd7TW5tQWvLrOhREGY"
+client_secret = "dwPtxqs6MN"
+
+x = 0
+userInput = '1'
+keyword = input("블로그 검색: ")
+while(userInput=='1'):
+    url = "https://openapi.naver.com/v1/search/blog?query="+ keyword + '&display=10'
+    headers = {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':client_secret}
+    params = {'keyword':keyword, 'display':10, 'start':1+x*10}
+    result = requests.get(url, headers = headers, params=params).json()
+
+    for item in result['items']:
+        print(item['title'].replace('<b>','').replace('</b>',''))
+        print(item['description'].replace('<b>','').replace('</b>',''))
+        print(item['link'])
+
+    x+=10
+    
+    userInput = input("다음 페이지는 1, 끝내시려면 0 ")
+    
+
---
+++ newsSearch.py
@@ -0,0 +1,26 @@
+import csv
+import os
+import sys
+import requests 
+import time
+client_id = "INgd7TW5tQWvLrOhREGY"
+client_secret = "dwPtxqs6MN"
+
+keyword = input("검색하고 싶은 뉴스를 입력하세요: ") 
+url = "https://openapi.naver.com/v1/search/news.json"
+headers = {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':client_secret}
+
+display_cnt = 100
+
+for x in range(10):
+    params = {'query':keyword, 'display':100, 'sort':'sim', 'start':x*display_cnt+1}
+    result = requests.get(url, headers = headers, params=params).json()
+    time.sleep(1)
+    for i, item in enumerate(result['items']):
+        print(i+1+x*display_cnt, " 번째")
+        print(item['title'].replace('<b>','').replace('</b>',''))
+        print(item['pubDate'][:-5])
+        print(item['link'])
+
+        
+
